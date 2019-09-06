eff370094758228fc14af5d4d472086fc47754b9
Jvymvn <Jordon.carter@yahoo.com>
2019-09-06 11:38:01 -0400

    Updates

---
+++ petful-client/components/Onboard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Onboard() {
+    return (
+        <h1>Petful</h1>
+        <p>Looking for a dog or a cat to adopt? Welcome to Petful! Here at Petful</p>
+    )
+}
